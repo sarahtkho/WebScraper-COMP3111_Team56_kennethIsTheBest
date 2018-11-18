fd763fa6c7113ad0437b060cd0b1792784f3cc2d
johnn <johnn@LAPTOP-5576OS1A>
2018-11-18 17:57:50 +0800

    more comments on Item.java

--- src/main/java/comp3111/webscraper/Item.java
+++ src/main/java/comp3111/webscraper/Item.java
@@ -14,32 +14,75 @@ public class Item implements Comparable<Item>{
 	private String url ;
 	private String postdate;
 	
+	/**
+	 * Get item title
+	 * @return item title
+	 */
 	public String getTitle() {
 		return title;
 	}
+	
+	/**
+	 * Set item title
+	 * @param title
+	 */
 	public void setTitle(String title) {
 		this.title = title;
 	}
+	
+	/**
+	 * Get item price
+	 * @return item price
+	 */
 	public double getPrice() {
 		return price;
 	}
+	
+	/**
+	 * Set item price
+	 * @param price
+	 */
 	public void setPrice(double price) {
 		this.price = price;
 	}
+	
+	/**
+	 * Get item url
+	 * @return item url
+	 */
 	public String getUrl() {
 		return url;
 	}
+	
+	/**
+	 * Set item url
+	 * @param url
+	 */
 	public void setUrl(String url) {
 		this.url = url;
 	}
+	
+	/**
+	 * Set item postdate
+	 * @param postdate
+	 */
 	public void setPostdate(String postdate) {
 		this.postdate = postdate;
 	}
+	
+	/**
+	 * Get item post date
+	 * @return
+	 */
 	public String getPostdate() {
 		return postdate;
 	}
 	
-	//Used to sort the Vector<Item> result
+	/**
+	 * Used to sort the Vector<Item> result
+	 * @param other
+	 * @return 1, -1 or 0 to indicate the compare result
+	 */
 	@Override
 	public int compareTo(Item other) {
 		if(this.price > other.price)
